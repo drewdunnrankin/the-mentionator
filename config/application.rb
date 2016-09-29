c388dd6ae68c921b54e71e445471bf9ae1f5e9de
Drew Dunn-Rankin <drewdunnrankin@gmail.com>
2016-09-29 11:50:43 -0500

    heroku 3

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,6 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.generators do |g|
-	g.orm :mongoid
-end
-
 module RihannasNightmare
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
