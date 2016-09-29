f0eeae011ebcfce4902a2a33910d8b0075a58db8
Drew Dunn-Rankin <drewdunnrankin@gmail.com>
2016-09-29 12:00:13 -0500

    Heroku testing 5

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Mongoid.load!('config/mongoid.yml')
+Mongoid.load!('config/mongoid.yml', :production)
 
 module RihannasNightmare
   class Application < Rails::Application
