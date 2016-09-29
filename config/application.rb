ab81b74b9caa26966aa917f4f7f6f5bd71be0ccd
Drew Dunn-Rankin <drewdunnrankin@gmail.com>
2016-09-29 11:48:17 -0500

    heroku testing pt 2

--- config/application.rb
+++ config/application.rb
@@ -16,14 +16,16 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.generators do |g|
+	g.orm :mongoid
+end
+
 module RihannasNightmare
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
 
-config.generators do |g|
-	g.orm :mongoid
-end
